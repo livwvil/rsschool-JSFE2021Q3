a27b9120ca8efebec206f6dbd0d783c437acdd4b
Roman Fomin <fomin.roman.andr@gmail.com>
2021-10-04 16:03:13 +0300

    fix: small fixes, preparing to adaptivity

--- museum-adaptive/src/ts/index.ts
+++ museum-adaptive/src/ts/index.ts
@@ -2,124 +2,138 @@ import "../sass/style.scss";
 
 function shuffleArray(array: any[]) {
   for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
 }
 
 export function randomizeImages() {
-  let images : NodeListOf<HTMLImageElement> = document.querySelectorAll(".art-gallery img")
+  let images: NodeListOf<HTMLImageElement> =
+    document.querySelectorAll(".art-gallery img");
   let imagesShuffled = Array.from(images);
   shuffleArray(imagesShuffled);
   imagesShuffled.forEach((image, idx) => {
-    image.src = `assets/img/galery${idx+1}.jpg`
-  })
+    image.src = `assets/img/galery${idx + 1}.jpg`;
+  });
 }
-randomizeImages()
+randomizeImages();
 
-let timeline: HTMLInputElement | null = document.querySelector(".progress.timeline")
+let timeline: HTMLInputElement | null =
+  document.querySelector(".progress.timeline");
 function timelineListener() {
-  let progress = timeline?.value
+  let progress = timeline?.value;
   let style = `linear-gradient(
     to right,
     #710707 0%,
     #710707 ${progress}%,
     #b3b3b3 ${progress}%,
     #b3b3b3 100%
-    )`
-    if(timeline)
-    timeline.style.background = style;
-  }
-  timeline?.addEventListener("input", timelineListener)
-  
-  let volume: HTMLInputElement | null = document.querySelector(".progress.volume")
-  function volumeListener() {
-    let progress = volume?.value
-    let style = `linear-gradient(
+    )`;
+  if (timeline) timeline.style.background = style;
+}
+timeline?.addEventListener("input", timelineListener);
+
+let volume: HTMLInputElement | null =
+  document.querySelector(".progress.volume");
+function volumeListener() {
+  let progress = volume?.value;
+  let style = `linear-gradient(
       to right,
       #710707 0%,
       #710707 ${progress}%,
       #b3b3b3 ${progress}%,
       #b3b3b3 100%
-      )`
-      if(volume)
-      volume.style.background = style;
-    }
-    volume?.addEventListener("input", volumeListener)
-    
-    const buyBtn: HTMLElement | null = document.querySelector(".buy-btn")
-    const closeBtn: HTMLElement | null = document.querySelector(".close-btn")
-    const modalBuy: HTMLElement | null = document.querySelector(".modal-buy") 
-    const toggleBuyModal = (e: MouseEvent) => {
-      if([modalBuy, buyBtn, closeBtn].includes(e.target as HTMLElement)) {
-        modalBuy?.classList.toggle("active")
-        e.stopPropagation()
-      }
-    }
-    buyBtn?.addEventListener("click", toggleBuyModal);
-    modalBuy?.addEventListener("click", toggleBuyModal);
-    closeBtn?.addEventListener("click", toggleBuyModal);
-    const bookBtn: HTMLElement | null = document.querySelector(".book-button")
-    bookBtn?.addEventListener('click', function (e) {
-      const x = e.clientX
-      const y = e.clientY
+      )`;
+  if (volume) volume.style.background = style;
+}
+volume?.addEventListener("input", volumeListener);
 
-      const buttonTop = this.getBoundingClientRect().y
-      const buttonLeft = this.getBoundingClientRect().x
-
-      const xInside = x - buttonLeft
-      const yInside = y - buttonTop
-
-      const circle = document.createElement('span')
-
-      circle.classList.add('circle')
-      circle.style.top = yInside + 'px'
-      circle.style.left = xInside + 'px'
-
-      this && this.appendChild(circle)
-      setTimeout(() => circle.remove(), 500)
-  })
-
-
-    let result = {
-      "Вёрстка валидная +10": 10,
-      "<header>, <main>, <footer> + 2": 2,
-      "семь элементов <section> (по количеству секций) +2": 2,
-      "только один заголовок <h1> +2": 2,
-      "семь заголовков <h2> (по количеству секций) +2": 2,
-      "шесть заголовков <h3> (по количеству карточек) +2": 2,
-      "два элемента <nav> (основная и вспомогательная панель навигации) +2": 2,
-      "три списка ul > li > a (основная и вспомогательная панель навигации, ссылки на соцсети) +2": 0,
-      "тринадцать кнопок button (четыре из них в секции Video, пять в секции Tickets, по две - стрелки слайдера и плейлиста) +2": 2,
-      "три тега input type='radio' (в секции Tickets) +2": 2,
-      "два тега input type='number'(в секции Tickets) +2": 2,
-      "два тега input type='range' (громкось и прогрес-бар видео) +2": 2,
-      "для всех элементов <img> указан обязательный атрибут alt +2": 2,
-      "Вёрстка соответствует макету +45": 45,
-      "Форма покупки билетов +22": 0,
-      "добавлен favicon +2": 2,
-      "для построения сетки используются флексы или гриды +2": 2,
-      "при уменьшении масштаба страницы браузера вёрстка размещается по центру, а не сдвигается в сторону +2": 2,
-      "фоновый цвет каждого блока и секции тянется на всю ширину страницы +2": 2,
-      "иконки добавлены в формате .svg. SVG может быть добавлен любым способом. Обращаем внимание на формат, а не на способ добавления +2": 2,
-      "расстояние между буквами, там, где это требуется по макету, регулируется css-свойством letter-spacing +2": 2,
-      "переключаются радиокнопки в блоке Tickets, одновременно может быть выбрана только одна кнопка +2": 2,
-      "в блоке Contacts правильно указанны ссылки на почту mailto и на телефон tel +2": 2,
-      "в футере добавлены ссылки на соцсети. Круглая граница вокруг иконок соцсетей выполнена при помощи css +2": 1,
-      "плавная прокрутка по якорям +5": 5,
-      "параллакс +5": 5,
-      "при кликам по кнопке Discover the Louvre и карточкам секции Visiting открываются полноэкранные панорамы Google Street View встроенные в страницы вашего сайта при помощи iframe +5": 0,
-      "интерактивность включает в себя не только изменение внешнего вида курсора, например, при помощи свойства cursor: pointer, но и другие визуальные эффекты – изменение цвета фона или шрифта, появление подчёркивания и т.д. Если в макете указаны стили при наведении и клике, для элемента указываем эти стили. Если в макете стили не указаны, реализуете их по своему усмотрению, руководствуясь общим стилем макета +4": 0,
-      "обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы +2": 2,
-      "интерактивность при наведении карточек в секции Visiting предусматривает плавное растягивание подчёркивания заголовка карточки на всю ширину карточки Демо +2": 2,
-      "интерактивность при наведении иконок социальных сетей в футере предусматривает изменение цвета иконки и круглой границы вокруг иконки на золотистый +2": 2,
-  "можно передвигать ползунки громкости и прогресс-бар видео, при этом цвет шкалы до и после ползунка отличается и соответствует макету +2": 2,
-  "кликами по кнопкам + и - в секции Tiskets можно менять количество билетов Basic и Senior от 0 до 20 +2": 2,
-  "кнопке Book' в форме покупки билетов добавлен ripple-эффект Демо +2": 0,
-  "при перезагрузке (обновлении) страницы картины в блоке Galery отображаются в рандомном порядке + 10": 10,
+const buyBtn: HTMLElement | null = document.querySelector(".buy-btn");
+const closeBtn: HTMLElement | null = document.querySelector(".close-btn");
+const modalBuy: HTMLElement | null = document.querySelector(".modal-buy");
+const toggleBuyModal = (e: MouseEvent) => {
+  if ([modalBuy, buyBtn, closeBtn].includes(e.target as HTMLElement)) {
+    modalBuy?.classList.toggle("active");
+    e.stopPropagation();
+  }
 };
+buyBtn?.addEventListener("click", toggleBuyModal);
+modalBuy?.addEventListener("click", toggleBuyModal);
+closeBtn?.addEventListener("click", toggleBuyModal);
+const bookBtn: HTMLElement | null = document.querySelector(".book-button");
+bookBtn?.addEventListener("click", function (e) {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const buttonTop = this.getBoundingClientRect().y;
+  const buttonLeft = this.getBoundingClientRect().x;
+
+  const xInside = x - buttonLeft;
+  const yInside = y - buttonTop;
+
+  const circle = document.createElement("span");
+
+  circle.classList.add("circle");
+  circle.style.top = yInside + "px";
+  circle.style.left = xInside + "px";
+
+  this && this.appendChild(circle);
+  setTimeout(() => circle.remove(), 500);
+});
+
+let result = {
+  "(1024) Блок header +4": 0,
+  "(1024) Секция Welcome +4": 0,
+  "(1024) Секция Visiting +4": 0,
+  "(1024) Секция Explore +4": 0,
+  "(1024) Секция Video +4": 0,
+  "(1024) Секция Gallery +4": 0,
+  "(1024) Секция Tickets +4": 0,
+  "(1024) Форма покупки билетов +4": 0,
+  "(1024) Секция Contacts +4": 0,
+  "(1024) Блок footer +4": 0,
+  "(768) Блок header +4": 0,
+  "(768) Секция Welcome +4": 0,
+  "(768) Секция Visiting +4": 0,
+  "(768) Секция Explore +4": 0,
+  "(768) Секция Video +4": 0,
+  "(768) Секция Gallery +4": 0,
+  "(768) Секция Tickets +4": 0,
+  "(768) Форма покупки билетов +4": 0,
+  "(768) Секция Contacts +4": 0,
+  "(768) Блок footer +4": 0,
+  "(420) Блок header +4": 0,
+  "(420) Секция Welcome +4": 0,
+  "(420) Секция Visiting +4": 0,
+  "(420) Секция Explore +4": 0,
+  "(420) Секция Video +4": 0,
+  "(420) Секция Gallery +4": 0,
+  "(420) Секция Tickets +4": 0,
+  "(420) Форма покупки билетов +4": 0,
+  "(420) Секция Contacts +4": 0,
+  "(420) Блок footer +4": 0,
+  "Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +6": 0,
+  "При изменении ширины экрана плавно изменяются размеры: слайдера в секции Welcome +2": 0,
+  "При изменении ширины экрана плавно изменяются размеры: слайдера сравнения изображений в секции Explore +2": 0,
+  "При изменении ширины экрана плавно изменяются размеры: кастомного видеоплеера в секции Video +2": 0,
+  "При изменении ширины экрана плавно изменяются размеры: слайдера в секции Video +2": 0,
+  "При изменении ширины экрана плавно изменяются размеры: YouTube-видео в плейлисте в секции Video, маленькие видео выровнены по краям большого +2": 0,
+  "При изменении ширины экрана плавно изменяются размеры: галереи изображений и изображений в ней +2": 0,
+  "При изменении ширины экрана плавно изменяются размеры: карты +2": 0,
+  "На ширине экрана 1024рх и меньше реализовано адаптивное меню +12": 0,
+  "при нажатии на бургер-иконку меню появляется, плавно выдвигаясь слева, бургер-иконка изменяется на крестик. При нажатии на крестик меню исчезает, плавно возвращаясь назад, иконка крестика превращается в бургер-иконку +2": 0,
+  "ссылки в меню работают, обеспечивая плавную прокрутку по якорям +2": 0,
+  "при клике по ссылке в адаптивном меню, или при клике по любому месту сайта, кроме самого адаптивного меню, меню закрывается +2": 0,
+  "вёрстка меню соответствует макету на всех проверяемых разрешениях +6": 0,
+  "Оптимизация скорости загрузки страницы +8": 0,
+};
+/*
+0 to 49 (red): Poor - не выполнено, 0 баллов
+50 to 89 (orange): Needs Improvement - частично выполнено +4
+90 to 100 (green): Good - выполнено полностью +8
+*/
 let sum = 0;
 for (let i of Object.entries(result)) {
   console.log(i[1] + "\t" + i[0]);
