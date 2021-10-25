e485053846193aa4d01db10124c22e6cb495c873
Roman Fomin <fomin.roman.andr@gmail.com>
2021-10-25 20:55:05 +0300

    fix: translate, github random, audio highliting

--- momentum/src/ts/githubAPI.ts
+++ momentum/src/ts/githubAPI.ts
@@ -1,24 +1,10 @@
+export async function* getNextUrl(amount: number, currentTimeOfDay: string) {
+  let cur = Math.ceil(Math.random() * (amount - 1));
 
-function shuffleArray(array: any[]) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-}
-
-export async function* getNextUrl (amount: number, currentTimeOfDay: string) {
-  const indices: number[] = [];
-  for (let i = 0; i < amount; i++) {
-    indices.push(i);
-  }
-
-  shuffleArray(indices);
-
-  for (let i = 0; i < amount; i++) {
-    const imageNumberStr = (indices[i] + 1).toString().padStart(2, "0");
+  while (true) {
+    const imageNumberStr = ((cur % amount) + 1).toString().padStart(2, "0");
+    cur = cur > amount ? 0 : cur + 1;
     yield `https://raw.githubusercontent.com/livwvil/stage1-tasks/assets/images/${currentTimeOfDay}/${imageNumberStr}.jpg`;
   }
   return null;
-};
+}
