4389217a4d26f905850730f0199d481a783a0c80
Roman Fomin <fomin.roman.andr@gmail.com>
2021-11-22 21:29:35 +0300

    fix: rename components -_-

--- art-quiz/src/constants.js
+++
@@ -1,6 +0,0 @@
-export const QUIZ_CATEGORIES_AMOUNT = 12; // max 12 with current dataset
-export const QUIZ_QUESTIONS_AMOUNT = 10; // max 10 with current dataset
-export const ARTIST_QUIZ = 'artist_quiz';
-export const PICTURE_QUIZ = 'picture_quiz';
-export const DEFAULT_VOLUME = 35;
-export const DEFAULT_TIME_TO_ANSWER = 7;
