d83af55d2534b8fd45c403327911a8819b3fe314
Roman Fomin <fomin.roman.andr@gmail.com>
2021-12-08 15:52:03 +0300

    feat: add adaptivity

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,8 +2,9 @@ import { Loader } from './loader';
 
 class AppLoader extends Loader {
     public constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '734b317b7e464451b55540a26ac62b65',
+        super('https://nodenews.herokuapp.com/', {
+            apiKey: '88c9d73f48d8415585b882260bc26516',
+            // apiKey: '734b317b7e464451b55540a26ac62b65',
         });
     }
 }
--- migration-to-typescript/src/index.ts
+++ migration-to-typescript/src/index.ts
@@ -1,5 +1,21 @@
 import App from './components/app/app';
 import './global.css';
 
+const burgerBtn: HTMLElement | null = document.querySelector('.burger');
+const navMenu: HTMLElement | null = document.querySelector('.sources');
+
+const mobileMenuToggler = (e: MouseEvent) => {
+    const burgerClasses = burgerBtn?.classList;
+    burgerClasses?.toggle('active');
+
+    const menuClasses = navMenu?.classList;
+    menuClasses?.toggle('active');
+
+    e.stopPropagation();
+};
+
+navMenu.addEventListener('click', mobileMenuToggler);
+burgerBtn.addEventListener('click', mobileMenuToggler);
+
 const app = new App();
 app.start();
