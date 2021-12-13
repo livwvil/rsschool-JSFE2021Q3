a17783e073b1acfc8c24a96a928c32178ad2ae88
Roman Fomin <fomin.roman.andr@gmail.com>
2021-12-13 13:20:22 +0300

    fix: revert devserver, coz new works not correct

--- christmas-task/webpack.dev.config.js
+++ christmas-task/webpack.dev.config.js
@@ -3,4 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, '../dist'),
+    },
 };
